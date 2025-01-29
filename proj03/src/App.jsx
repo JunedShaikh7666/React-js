e12caba33d4db1057ae2b24c0b2e2c88a56b0a48
JunedShaikh7666 <junuu7666@gmail.com>
2025-01-29 15:21:09 +0530

    basicProject

--- proj03/src/App.jsx
+++ proj03/src/App.jsx
@@ -1,25 +1,21 @@
-import { useState } from 'react'
 import './App.css'
+import { useState } from 'react'
 
-function App() {
-  let [color,setcolor] = useState();
-  let changecolor = ()=>{
-  }
-  
+ function App() {
+  let [color, setColor] = useState('');
+
 
   return (
     <>
-      <div className="menu " >
-        <ul>
-          <button onChange={color} className='red'>Red</button>
-          <button onChange={color} className='yellow'>Yellow</button>
-          <button onChange={color} className='green'>Green</button>
-          <button onChange={color} className='blue'>Blue</button>
-          <button onChange={color} className='peach'>peach</button>
-          <button onChange={color}className='lavender'>lavender</button>
-        </ul>
-        </div>      
+    <div style={{backgroundColor: color}}>
+      <button onClick={()=> {setColor("red")}}>Red</button>
+      <button onClick={()=> {setColor("green")}}>Green</button>
+      <button onClick={()=> {setColor("blue")}}>Blue</button>
+      <button onClick={()=> {setColor("lavender")}}>Lavender</button>
+      <button onClick={()=> {setColor("peachpuff")}}>Peach</button>
+    </div>   
     </>
   )
 }
+
 export default App
