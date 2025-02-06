f397eb90d001a841e441ad4f2433a0ce3a2e79cc
JunedShaikh7666 <junuu7666@gmail.com>
2025-02-06 17:53:07 +0530

    BasicProject

--- Password-generator/src/App.jsx
+++ Password-generator/src/App.jsx
@@ -1,24 +1,127 @@
-
-import { use } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [length,setLength] = useState(20);
-  const [number, setNumber] = useState(false);
-  const [charAllow, setCharAllow] = useState(false);
-  const [Password, setPassword] = useState();
+  const [length, setLength] = useState(8);
+  const [numberAllow, setNumberAllow] = useState(false);
+  const [charAllow, setcharAllow] = useState(false);
+  const [password,setPassword] = useState("");
+  const [isEditable,setIsEditable] = useState(false)
 
-  const passwordGenerator = () =>{
-    
-  }
+  //UseRef hook use here.
+  const passwordRef = useRef(null);
 
+  const passwordGenerator =useCallback(() => {
+    let pass = ''
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+
+    if(numberAllow) str += '0123456789';
+    if(charAllow) str += '!@#$%&*_';
+
+    for(let i = 1; i<=length;i++){
+      let char = Math.floor(Math.random()*str.length+1)
+      pass += str.charAt(char);
+    }
+    setPassword(pass)
+  },[length,numberAllow,charAllow,setPassword])
+
+  const copyTextToClipBoard = useCallback(()=>{
+    passwordRef.current?.select()
+    window.navigator.clipboard.writeText(password)
+  },[password]);
+
+  const handleInputChange = (e) => {
+    if (isEditable) {
+      setPassword(e.target.value); 
+    }
+  };
+
+  const setInput = useCallback(() => {
+    setPassword(''); 
+    setIsEditable(true); 
+    passwordRef.current?.focus(); 
+  }, []);
+  
+  //UseEffect Hook use here
+  useEffect(()=>{
+    passwordGenerator()
+  },[length,numberAllow,charAllow, passwordGenerator])
+  
   return (
     <>
-      <h1 className='text-4xl text-center text-white'>Password Generator</h1>
-      <div className='' style={{backgroundColor: "gray", width: '50%', margin: 'auto', padding: '20px', borderRadius: '10px'}}>
-        <label className='text-xl bg-white weidth-100'>Hello</label><br />
-      <input type="range" style={{width:'100', color:'white'}}/>
+      
+      <div className='w-full max-w-md  mx-auto mt-30 shadow-md rounded-lg px-4 py-8 text-orange-500 bg-gray-800'>
+        <h1 className='text-4xl text-center text-white mb-10'>Password Generator</h1>
+        
+        <div className="flex shadow rounded-lg overflow-hidden mb-4">
+          <input 
+          type="text"
+          onChange={handleInputChange}
+          value={password}
+          className='outline-none w-full py-1 px-3 bg-white'
+          placeholder='Password'
+          readOnly={!isEditable}
+          ref={passwordRef}
+          />
+          <button 
+          id='copy'
+          onClick={copyTextToClipBoard}
+          className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'          
+          >Copy</button>
+        </div>
+
+        <div className="flex text-sm gap-x-2">
+          <div className="flex items-center gap-x-1">
+            <input 
+            type="range"
+            min={8}
+            max={20}
+            value={length}
+            className='coursor-pointer'
+            onChange={(e)=>{setLength(e.target.value)}}
+            />
+            <label>Length : {length}</label>
+          </div>
+          <div className="flex items-center gap-x-1">
+            <input 
+            type="checkbox"
+            defaultChecked ={numberAllow}
+            id='numInput'
+            onChange={()=>{setNumberAllow((prev)=>!prev);
+            }}
+            />
+            <label htmlFor="numInput">+Numbers</label>
+          </div>
+          <div className="flex items-center gap-x-1">
+            <input 
+            type="checkbox"
+            defaultChecked ={charAllow}
+            id='charInput'
+            onChange={()=>{setcharAllow((prev) => !prev);
+            }}
+            />
+            <label htmlFor="charInput">+Charecters</label>
+          </div>
+
+        </div>
+        <div className="flex sm:justify-center gap-2">
+
+          <div onClick={setInput}>
+            <button id='newpass' className='border-1 w-50 h-8 mt-5'>Create your Own</button>
+          </div>
+          
+          <div onClick={passwordGenerator}>
+            <button id='resetpass' className='border-1 w-50 h-8 mt-5'>Reset / Refresh</button>
+          </div>
+        
+        </div>
+
       </div>
+
+      <div className='w-full max-w-md  mx-auto shadow-md rounded-lg px-4 py-8 text-orange-500 bg-gray-800'>
+        <textarea  className='bg-white w-full m-0 p-10' placeholder="Write you'r Copied Password..."></textarea>
+      </div>
+
     </>
   )
 }
